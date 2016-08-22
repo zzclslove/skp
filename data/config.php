8d7706f13f7af6ebc5fea82c2274fd2268323a0a
zzclslove <zzclslove@sina.com>
2016-08-22 19:18:40 +0800

    Initial commit

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "mysql.sql132.eznowdata.com:3306";
+
+// database name
+$db_name   = "sq_lanbeike";
+
+// database username
+$db_user   = "sq_lanbeike";
+
+// database password
+$db_pass   = "zzc79801890";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','0c92f01f4673a2eca47b6f92a43cba30');
+
+?>
