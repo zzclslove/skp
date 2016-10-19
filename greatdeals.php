a58e4f62ce7fa79c403e1acd3ba3cdea0ca7e317
zzclslove <zzclslove@sina.com>
2016-10-19 19:08:39 +0800

    u

---
+++ greatdeals.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/19
+ * Time: 12:46
+ */
+define('IN_ECS', true);
+require(dirname(__FILE__) . '/includes/init.php');
+
+$smarty->display('greatdeals.dwt');
